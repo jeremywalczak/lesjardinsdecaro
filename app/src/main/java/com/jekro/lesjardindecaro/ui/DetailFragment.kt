0a1ee956c68c73705d18478b6b54a60c72445500
Kromei <dav.bastien@gmail.com>
2020-04-19 16:24:18 +0200

    rryr

--- app/src/main/java/com/jekro/lesjardindecaro/ui/DetailFragment.kt
+++ app/src/main/java/com/jekro/lesjardindecaro/ui/DetailFragment.kt
@@ -1,12 +1,16 @@
 package com.jekro.lesjardindecaro.ui
 
 import android.os.Bundle
+import android.view.Gravity
 import com.auchan.uikit.module.ModuleInteractor
 import com.auchan.uikit.mvp.AbsFragment
 import com.jekro.lesjardindecaro.R
 import com.jekro.lesjardindecaro.model.Configuration
 import com.jekro.lesjardindecaro.model.Product
 import com.jekro.lesjardindecaro.ui.home.HomePageContract
+import kotlinx.android.synthetic.main.activity_home_page.*
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_homepage.*
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 
@@ -14,6 +18,16 @@ class DetailFragment : AbsFragment<HomePageContract.View, HomePageContract.Prese
 HomePageContract.View {
 
     override fun displayResult(configuration: Configuration) {
+        var numberProduct = product_number.toString().toInt()
+        minus?.setOnClickListener {
+            numberProduct -= 1
+            product_number.setText(numberProduct)
+        }
+        plus?.setOnClickListener {
+            numberProduct += 1
+            product_number.setText(numberProduct)
+
+        }
     }
 
 
