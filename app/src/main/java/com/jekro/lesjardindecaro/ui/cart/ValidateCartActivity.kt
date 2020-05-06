dd767ac53a73f3c5ed3fc83158324640db139cb7
Jérémy Walczak <jwalczak@partner.auchan.com>
2020-05-06 17:59:48 +0200

    adjute validate view

--- app/src/main/java/com/jekro/lesjardindecaro/ui/cart/ValidateCartActivity.kt
+++ app/src/main/java/com/jekro/lesjardindecaro/ui/cart/ValidateCartActivity.kt
@@ -1,10 +1,15 @@
 package com.jekro.lesjardindecaro.ui.cart
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.DatePicker
+import android.widget.TextView
 import android.widget.TimePicker
 import androidx.appcompat.app.AppCompatActivity
 import com.jekro.lesjardindecaro.R
+import kotlinx.android.synthetic.main.activity_validate_cart.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ValidateCartActivity : AppCompatActivity() {
@@ -22,5 +27,18 @@ class ValidateCartActivity : AppCompatActivity() {
         datePicker.minDate = cal.timeInMillis
         val timePicker = findViewById<TimePicker>(R.id.timePicker)
         timePicker.setIs24HourView(true)
+
+        /*datePicker.setOnTouchListener (object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when (event?.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        /*val myDate = SimpleDateFormat("dd-MM-yyyy").parse(datePicker.dayOfMonth+"-"+datePicker.month+"-"+datePicker.year);
+                        val simpleDateFormat = SimpleDateFormat("EEEE")
+                        val dayName = simpleDateFormat.format(myDate)*/
+                    }
+                }
+                return v?.onTouchEvent(event) ?: true
+            }
+        })*/
     }
 }
